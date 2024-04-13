9ea53c72678d209d73772a7b8323e772f2314178
Cassius Villareal <cassiusfive@gmail.com>
2024-04-13 15:56:04 -0700

    🔥 Firebase init
    british commit

---
+++ client/app/config/firebase.ts
@@ -0,0 +1,12 @@
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+    apiKey: "AIzaSyCFq_mZOScJwWdqhOL98uPbLjD7-njuFys",
+    authDomain: "snapmenu-meow.firebaseapp.com",
+    projectId: "snapmenu-meow",
+    storageBucket: "snapmenu-meow.appspot.com",
+    messagingSenderId: "203685616343",
+    appId: "1:203685616343:web:01da3939e00c900899ae92",
+};
+
+const app = initializeApp(firebaseConfig);
