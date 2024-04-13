9c357f8b8ee7d65d886e7cdb9c67f1a8be27abb6
Cassius Villareal <cassiusfive@gmail.com>
2024-04-13 00:39:52 -0700

    🚧 Clerk Integration
    implemented basic user authentication

---
+++ client/app/(auth)/home.tsx
@@ -0,0 +1,10 @@
+import { StyleSheet, Text, View } from "react-native";
+import React from "react";
+
+const Home = () => {
+    return (
+        <View>
+            <Text>Home</Text>
+        </View>
+    );
+};
--- client/app/_layout.tsx
+++ client/app/_layout.tsx
@@ -1,58 +1,58 @@
-import FontAwesome from '@expo/vector-icons/FontAwesome';
-import { DarkTheme, DefaultTheme, ThemeProvider } from '@react-navigation/native';
-import { useFonts } from 'expo-font';
-import { Stack } from 'expo-router';
-import * as SplashScreen from 'expo-splash-screen';
-import { useEffect } from 'react';
+import { ClerkProvider, useAuth } from "@clerk/clerk-expo";
+import { Slot, useRouter, useSegments } from "expo-router";
+import { useEffect } from "react";
+import * as SecureStore from "expo-secure-store";
 
-import { useColorScheme } from '@/components/useColorScheme';
+const CLERK_PUBLISHABLE_KEY = process.env.EXPO_PUBLIC_CLERK_PUBLISHABLE_KEY;
 
-export {
-  // Catch any errors thrown by the Layout component.
-  ErrorBoundary,
-} from 'expo-router';
-
-export const unstable_settings = {
-  // Ensure that reloading on `/modal` keeps a back button present.
-  initialRouteName: '(tabs)',
+const tokenCache = {
+    async getToken(key: string) {
+        try {
+            return SecureStore.getItemAsync(key);
+        } catch (err) {
+            return null;
+        }
+    },
+    async saveToken(key: string, value: string) {
+        try {
+            return SecureStore.setItemAsync(key, value);
+        } catch (err) {
+            return;
+        }
+    },
 };
 
-// Prevent the splash screen from auto-hiding before asset loading is complete.
-SplashScreen.preventAutoHideAsync();
+const InitialLayout = () => {
+    const { isLoaded, isSignedIn } = useAuth();
+    const segments = useSegments();
+    const router = useRouter();
 
-export default function RootLayout() {
-  const [loaded, error] = useFonts({
-    SpaceMono: require('../assets/fonts/SpaceMono-Regular.ttf'),
-    ...FontAwesome.font,
-  });
+    useEffect(() => {
+        if (!isLoaded) return;
 
-  // Expo Router uses Error Boundaries to catch errors in the navigation tree.
-  useEffect(() => {
-    if (error) throw error;
-  }, [error]);
+        const inTabsGroup = segments[0] === "(auth)";
 
-  useEffect(() => {
-    if (loaded) {
-      SplashScreen.hideAsync();
-    }
-  }, [loaded]);
+        console.log("User changed: ", isSignedIn);
 
-  if (!loaded) {
-    return null;
-  }
+        if (isSignedIn && !inTabsGroup) {
+            router.replace("/home");
+        } else if (!isSignedIn) {
+            router.replace("/login");
+        }
+    }, [isSignedIn]);
 
-  return <RootLayoutNav />;
-}
+    return <Slot />;
+};
 
-function RootLayoutNav() {
-  const colorScheme = useColorScheme();
+const RootLayout = () => {
+    return (
+        <ClerkProvider
+            publishableKey={CLERK_PUBLISHABLE_KEY!}
+            tokenCache={tokenCache}
+        >
+            <InitialLayout />
+        </ClerkProvider>
+    );
+};
 
-  return (
-    <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
-      <Stack>
-        <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
-        <Stack.Screen name="modal" options={{ presentation: 'modal' }} />
-      </Stack>
-    </ThemeProvider>
-  );
-}
+export default RootLayout;
