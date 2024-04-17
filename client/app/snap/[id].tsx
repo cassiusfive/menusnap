8b8f460bb1154a5dfa96d1ee1e7ddcbf45334086
Cassius Villareal <cassiusfive@gmail.com>
2024-04-17 02:20:26 -0700

    added ATC functionality

--- client/app/snap/[id].tsx
+++ client/app/snap/[id].tsx
@@ -1,4 +1,12 @@
-import { Text, View } from "react-native";
+import {
+    Pressable,
+    ScrollView,
+    StyleSheet,
+    Text,
+    View,
+    ActivityIndicator,
+    Dimensions,
+} from "react-native";
 import { useLocalSearchParams, Stack } from "expo-router";
 import { useEffect, useState } from "react";
 import {
@@ -13,14 +21,54 @@ import { db } from "../services/firebase";
 import Card from "@/shared/Card";
 
 type Item = {
-    name: String;
+    name: string;
+    description: string;
     price: number;
+};
+
+type MenuItemProps = {
+    item: Item;
+    quantity: number;
+    addToCart: () => void;
+    removeFromCart: () => void;
+};
+
+const MenuItem = (props: MenuItemProps) => {
+    const handlePress = (event: any) => {
+        const x = event.nativeEvent.locationX;
+        if (x < Dimensions.get("window").width / 2) {
+            props.removeFromCart();
+        } else {
+            props.addToCart();
+        }
+    };
+    return (
+        <Pressable onPress={handlePress}>
+            <Card style={{ padding: 16, gap: 5 }}>
+                <View style={styles.itemLabelsContainer}>
+                    <Text style={styles.itemHeader}>{props.item.name}</Text>
+                    {props.quantity > 0 ? (
+                        <View style={styles.quantityLabelContainer}>
+                            <Text style={styles.quantityLabel}>
+                                +{props.quantity}
+                            </Text>
+                        </View>
+                    ) : null}
+                    <Text style={styles.itemPrice}>
+                        ${props.item.price.toFixed(2)}
+                    </Text>
+                </View>
+                <Text style={{ padding: 0 }}>{props.item.description}</Text>
+            </Card>
+        </Pressable>
+    );
 };
 
 const Business = () => {
     const { id } = useLocalSearchParams<{ id: string }>();
     const [businessName, setBusinessName] = useState<String>("Loading...");
     const [items, setItems] = useState<Item[]>([]);
+    const [quantities, setQuantities] = useState<number[]>([]);
 
     const getBusinessInfo = async () => {
         const businessRef = doc(db, "businesses", id);
@@ -35,8 +83,9 @@ const Business = () => {
         const itemsSnapshot = await getDocs(itemsQuery);
         const itemsData = itemsSnapshot.docs.map((doc) => doc.data() as Item);
 
-        setItems(itemsData);
         setBusinessName(business.name);
+        setItems(itemsData);
+        setQuantities(new Array(itemsData.length).fill(0));
     };
 
     useEffect(() => {
@@ -45,11 +94,97 @@ const Business = () => {
         });
     }, []);
 
+    if (items.length === 0) {
+        return (
+            <>
+                <Stack.Screen options={{ title: "Loading..." }} />
+                <View
+                    style={{
+                        flex: 1,
+                        justifyContent: "center",
+                        alignItems: "center",
+                    }}
+                >
+                    <ActivityIndicator />
+                    <Text style={{ margin: 24 }}>Getting menu items</Text>
+                </View>
+            </>
+        );
+    }
+
+    const updateQuantity = (num: number, index: number) => {
+        const newQuantities = [...quantities];
+        let newQuantity = newQuantities[index] + num;
+        if (newQuantity < 0) {
+            newQuantity = 0;
+        }
+        newQuantities[index] = newQuantity;
+        setQuantities(newQuantities);
+    };
+
     return (
-        <View>
+        <>
             <Stack.Screen options={{ title: businessName as string }} />
-        </View>
+            <ScrollView style={{ padding: 10 }}>
+                {items.length
+                    ? items.map((item, index) => {
+                          return (
+                              <MenuItem
+                                  key={index}
+                                  item={item}
+                                  quantity={quantities[index]}
+                                  addToCart={() => {
+                                      updateQuantity(1, index);
+                                  }}
+                                  removeFromCart={() => {
+                                      updateQuantity(-1, index);
+                                  }}
+                              />
+                          );
+                      })
+                    : null}
+            </ScrollView>
+            <View style={styles.peepingContainer}></View>
+        </>
     );
 };
+
+const styles = StyleSheet.create({
+    quantityLabelContainer: {
+        marginHorizontal: 12,
+        backgroundColor: "black",
+        paddingHorizontal: 8,
+        borderRadius: 5,
+        alignSelf: "center",
+    },
+    quantityLabel: {
+        color: "white",
+        fontWeight: "bold",
+        fontSize: 18,
+    },
+    itemLabelsContainer: {
+        width: "auto",
+        flexDirection: "row",
+        alignItems: "baseline",
+    },
+    itemHeader: {
+        fontWeight: "bold",
+        fontSize: 20,
+        textAlign: "left",
+    },
+    itemPrice: {
+        color: "#27ae60",
+        fontWeight: "bold",
+        fontSize: 16,
+        textAlign: "right",
+        flex: 1,
+    },
+    peepingContainer: {
+        backgroundColor: "#27ae60",
+        height: 100,
+        borderTopLeftRadius: 20,
+        borderTopRightRadius: 20,
+    },
+});
 
 export default Business;
